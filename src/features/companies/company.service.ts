67961f5ce7b646e12924ab85db7c97e90a4d0cd8
fthomasvp <fellipecab@gmail.com>
2022-07-05 01:18:30 -0300

    build: add Axios dependency and improve services
    - update services to use Axios

--- src/features/companies/company.service.ts
+++ src/features/companies/company.service.ts
@@ -1,18 +1,22 @@
-import fetch from "cross-fetch";
+import axios from "axios";
 
 import { API_URL } from "../../constants";
+import { Company, CompanyNumber } from "./company.type";
+
+const axiosClient = axios.create({
+  baseURL: API_URL,
+});
 
 /** Get list of companies. */
-export const getCompaniesService = async (): Promise<Response> =>
-  await fetch(`${API_URL}/companies`);
+export const getCompaniesService = async () =>
+  await axiosClient.get<Company[]>(`${API_URL}/companies`);
 
 /** Get a specific company. */
-export const getCompanyByIdService = async (
-  companyId: string
-): Promise<Response> => await fetch(`${API_URL}/companies/${companyId}`);
+export const getCompanyByIdService = async (companyId: string) =>
+  await axiosClient.get<Company>(`${API_URL}/companies/${companyId}`);
 
 /** Get list of company numbers. */
-export const getNumbersByCompanyIdService = async (
-  companyId: string
-): Promise<Response> =>
-  await fetch(`${API_URL}/phone_numbers?company_id=${companyId}`);
+export const getNumbersByCompanyIdService = async (companyId: string) =>
+  await axiosClient.get<CompanyNumber[]>(
+    `${API_URL}/phone_numbers?company_id=${companyId}`
+  );
